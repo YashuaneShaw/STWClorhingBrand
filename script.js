befec55a83b2d1dbe4d88ddbed182e6bd4dcae3c
YashuaneShaw <yashaune@gmail.com>
2024-11-22 17:00:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,19 @@
 */
 
 
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const products = [
         {
